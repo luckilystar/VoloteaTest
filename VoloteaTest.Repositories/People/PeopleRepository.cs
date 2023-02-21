730457c3b700251f2c0147fa39890a64772e8492
Oscar Wongso <oscar.wongso@gmail.com>
2023-02-21 22:32:48 +0700

    Add Repository & Autofac

---
+++ VoloteaTest.Repositories/People/PeopleRepository.cs
@@ -0,0 +1,18 @@
+﻿using VoloteaTest.Core.Models.People;
+using VoloteaTest.EF;
+
+namespace VoloteaTest.Repository.People
+{
+    public class PeopleRepository:GenericRepository<Person>,IPeopleRepository
+    {
+        public PeopleRepository():base()
+        {
+            
+        }
+        public PeopleRepository(PeopleDbContext context)
+          : base(context)
+        {
+
+        }
+    }
+}
---
+++ VoloteaTest.Repositories/RepositoryModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoloteaTest.Repository
+{
+    public class RepositoryModule:Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.Load("VoloteaTest.Repository"))
+                   .Where(t => t.Name.EndsWith("Repository"))
+                   .AsImplementedInterfaces()
+                  .InstancePerLifetimeScope();
+        }
+    }
+}
