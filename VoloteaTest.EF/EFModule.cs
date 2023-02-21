cefcf91d065fb221dce554055f352f0c8047e596
Oscar Wongso <oscar.wongso@gmail.com>
2023-02-21 22:32:30 +0700

    Add EF Module for Autofac

---
+++ VoloteaTest.EF/EFModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoloteaTest.EF
+{
+    public class EFModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterModule(new RepositoryModule());
+            builder.RegisterType(typeof(PeopleDbContext))
+                .As(typeof(System.Data.Entity.DbContext))
+                .InstancePerLifetimeScope();
+        }
+
+    }
+}
