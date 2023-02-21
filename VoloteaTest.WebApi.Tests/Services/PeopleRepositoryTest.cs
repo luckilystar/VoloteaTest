b912f6589770ea92ac5834400fd3025947d83340
Oscar Wongso <oscar.wongso@gmail.com>
2023-02-21 20:49:05 +0700

    Add People Repository Test

--- VoloteaTest.WebApi.Tests/Services/PeopleRepositoryTest.cs
+++ VoloteaTest.WebApi.Tests/Services/PeopleRepositoryTest.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using VoloteaTest.Core.Models.People;
 using VoloteaTest.Service.People;
+using System.Linq;
 
 namespace VoloteaTest.WebApi.Tests.Services
 {
@@ -14,8 +16,57 @@ namespace VoloteaTest.WebApi.Tests.Services
             _peopleRepository = new PeopleRepository();
         }
         [TestMethod]
-        public void TestMethod1()
+        public void InsertPerson()
         {
+            Person p = new Person
+            {
+                Address = "Address 1",
+                Birthday = DateTime.Today.AddYears(-19),
+                Email = "address@gmail.com",
+                FirstName = "Name 1",
+                LastName = "Last Name 1",
+                PhoneNumber = "12345678"
+            };
+            var countBefore = _peopleRepository.GetAll().Count();
+            _peopleRepository.Insert(p);
+            _peopleRepository.Save();
+            var countAfter = _peopleRepository.GetAll().Count();
+            Assert.AreEqual(countBefore+1, countAfter);
+        }
+
+        [TestMethod]
+        public void UpdatePerson()
+        {
+            var person = _peopleRepository.GetAll().FirstOrDefault();
+            if (person != null)
+            {
+                person.Address = "This is the change of Address";
+                _peopleRepository.Update(person);
+                _peopleRepository.Save();
+                var personAfter = _peopleRepository.GetById(person.Id);
+                Assert.AreEqual(personAfter.Address, person.Address);
+            }
+            else
+            {
+                Assert.Fail("There is no person data to update.");
+            }
+        }
+
+        [TestMethod]
+        public void DeletePerson()
+        {
+            var person = _peopleRepository.GetAll().FirstOrDefault();
+            if (person != null)
+            {
+                _peopleRepository.Delete(person.Id);
+                _peopleRepository.Save();
+                var personAfter = _peopleRepository.GetById(person.Id);
+                Assert.IsNull(personAfter);
+            }
+            else
+            {
+                Assert.Fail("There is no person data to delete.");
+            }
         }
     }
 }
